dcac6bcbe538bfb81ade64db4e2132f2cd57f3ff
Ismoil <verylancer2@gmail.com>
2024-12-01 16:13:46 +0400

    Connected EndGameAnimation to button

--- Assets/Scripts/PuzzleButton.cs
+++ Assets/Scripts/PuzzleButton.cs
@@ -1,14 +1,18 @@
 public class PuzzleButton : Interactables
 {
     public string colorToRestore = string.Empty;
+    public EndGameAnimation endGameAnim;
 
     public override void Interact()
     {
         AudioManager.instance.PlaySFX("PuzzleFinish");
 
-        if (gameObject.name == "FinalPuzzleButton")
+        if (gameObject.name == "FinalPuzzleButton") {
             PuzzleManager.instance.CompletePuzzle();
-        else
+            if (endGameAnim != null) {
+                endGameAnim.StartEndScene();
+            }
+        } else
             PuzzleManager.instance.OnPuzzleCompletion(colorToRestore);
     }
 }
